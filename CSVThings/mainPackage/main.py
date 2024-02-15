4a1c643a96754f0f7582f9c10c811db99f98b85e
nicomp42 <nicholdw@ucmail.uc.edu>
2024-02-15 08:11:51 -0500

    First Commit

--- CSVThings/mainPackage/main.py
+++ CSVThings/mainPackage/main.py
@@ -4,3 +4,17 @@ from utilsPackage.utils import *
 
 # Call the function and store what it returns 
 #  in a variable called data
+if __name__ == "__main__":
+    data = read_CSV_file()
+    print("# of rows in dataset:", len(data))
+    # Print the first and last rows in data
+    print(data[0])
+    print(data[-1])
+    # I want a list comprehension expression
+    # to pull out all the collision Types into a set.
+    # Modify this expression to exclude the blank collision type
+    collisionTypes = {myData[6] for myData in data}
+    print("# of collision type:", len(collisionTypes))
+    print(collisionTypes)
+    
+    
